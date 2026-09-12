7101295e61c00a325b7f63cb4a0d20d99ee3e64c
domjanb97 <domjanb97@gmail.com>
2026-09-12 14:26:54 +0200

    User model is midified

--- UserAdmin/Models/User.cs
+++ UserAdmin/Models/User.cs
@@ -8,6 +8,7 @@ namespace UserAdmin.Models
     {
         public int? Id { get; set; }
         public string? Username { get; set; }
+        public string? Email { get; set; }
         public string? Password { get; set; }
         public DateTime RegisteredAt { get; set; }
     }
