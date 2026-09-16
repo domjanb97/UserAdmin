20c9bafdbe02dd3b4620d95f6f329be18916bfea
domjanb97 <domjanb97@gmail.com>
2026-09-16 14:39:39 +0200

    UserDBServices is created. ConnectionString is added

---
+++ UserAdmin/Services/UserDBService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserAdmin.Services
+{
+    class UserDBService
+    {
+        public string ConnectionString = "Server=localhost; Database=useradmin;User=root;Password=;";
+    }
+}
